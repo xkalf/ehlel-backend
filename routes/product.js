d1592dc4e7ebf8838989159210142bb4d928c0f1
xkalf <amroos988@gmail.com>
2022-03-30 16:19:31 +0800

    subfixes

--- routes/product.js
+++ routes/product.js
@@ -13,14 +13,7 @@ router.post("/", async (req, res) => {
 
 router.get("/", async (req, res) => {
   try {
-    const qNew = req.query.new;
-    const qCategory = req.query.category;
-    let products;
-    if (qNew) {
-      products = await Product.find().sort({ createdAt: -1 }).limit(20);
-    } else {
-      products = Product.find();
-    }
+    const products = await Product.find();
     res.status(200).json(products);
   } catch (err) {
     res.status(500).json(err);
@@ -49,10 +42,11 @@ router.put("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   try {
-    const deleted = Product.findByIdAndDelete(req.params.id);
+    const deleted = await Product.findByIdAndDelete(req.params.id);
     res.status(200).json(deleted);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
 module.exports = router;
